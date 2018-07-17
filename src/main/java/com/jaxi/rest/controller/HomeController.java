c3caf759d86f1e70ba64ba70086751d984816430
Dony Pramujo <donypramujo@gmail.com>
2018-07-17 11:48:17 +0700

    Fix Bugs v6

--- src/main/java/com/jaxi/rest/controller/HomeController.java
+++ src/main/java/com/jaxi/rest/controller/HomeController.java
@@ -6,11 +6,22 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.HashMap;
+
 @RestController
 public class HomeController {
 
     @RequestMapping("/")
-    public User profile( Authentication authentication){
+    public HashMap home(){
+
+        HashMap _map = new HashMap();
+        _map.put("timestamp",new Date());
+        return _map;
+    }
+
+    @RequestMapping("/profile")
+    public User profile(Authentication authentication){
 
         JaxiUserPrincipal _principal = (JaxiUserPrincipal) authentication.getPrincipal();
         User user  = _principal.getUser();
